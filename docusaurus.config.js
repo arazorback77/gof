c2bec0786f1ce35a59d6a3e905330f3151b99c05
arazorback77 <36630641+arazorback77@users.noreply.github.com>
2024-04-03 22:09:00 +0900

    noIndex false

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-  noIndex: true, // Defaults to `false`
+  noIndex: false, // Defaults to `false`
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
